eda3d51598008b3b49af6a9fe7bff6b63abf1ddc
Kyle Ross <6475128+kyledross@users.noreply.github.com>
2024-09-05 16:23:08 -0400

    Enhance UX with computer move feedback
    Added comments to simulate CPU thinking time and hover effect for better user experience. This provides visual indication of computer’s move, making the game more engaging and easier to follow for players.

--- src/main.py
+++ src/main.py
@@ -142,10 +142,10 @@ def computer_turn(current_player_id: int, opponent_player_id: int):
     possible_computer_turns = computer_logic.basic_strategy.find_best_moves(game_logic.board, current_player_id,
                                                                             opponent_player_id)
     best_computer_turn = computer_logic.basic_strategy.choose_one_best_move(possible_computer_turns)
-    pygame.time.wait(500)
+    pygame.time.wait(500) # give the illusion of thinking
     hovered_cell = best_computer_turn["x"], best_computer_turn["y"]
-    refresh_screen()
-    pygame.time.wait(500)
+    refresh_screen() # redraw with the "hovered" cell where the computer will "click"
+    pygame.time.wait(500) # give the human a chance to see where the computer is going to "click"
     game_logic.place_piece(best_computer_turn["x"], best_computer_turn["y"], current_player_id)
     while game_logic.process_board():
         # todo: implement animation here
